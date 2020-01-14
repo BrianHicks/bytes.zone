1f086497e957de4813d6028abeb6498b053292d5
Brian Hicks <brian@brianthicks.com>
2020-01-14 05:55:32 -0600

    add more deps

--- shell.nix
+++ shell.nix
@@ -1,10 +1,21 @@
 { ... }:
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs { };
+  nixpkgs = import sources.nixpkgs { config.allowUnfree = true; };
   niv = import sources.niv { };
+
+  # we need some additional header files on macos
+  macosDeps = [ nixpkgs.darwin.apple_sdk.frameworks.CoreServices ];
 in with nixpkgs;
 stdenv.mkDerivation {
   name = "bytes.zone";
-  buildInputs = [ niv.niv git nodePackages.npm ];
+  buildInputs = [
+    # base deps
+    niv.niv
+    git
+
+    # for elm-pages
+    nodejs
+    nodePackages.npm
+  ] ++ lib.optionals stdenv.isDarwin macosDeps;
 }
