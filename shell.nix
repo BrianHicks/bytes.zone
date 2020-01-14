98897f28e90d866f6c399065473bd3fab418bebd
Brian Hicks <brian@brianthicks.com>
2020-01-14 05:46:48 -0600

    nixify

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { };
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "bytes.zone";
+  buildInputs = [ niv.niv git nodePackages.npm ];
+}
