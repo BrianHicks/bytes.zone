a58e8f9fd6d2ec45bd5543b3f648f1d372fd9310
Brian Hicks <brian@brianthicks.com>
2020-01-17 16:17:53 -0600

    figure out the linear-gradient for the underline

--- src/HomePage.elm
+++ src/HomePage.elm
@@ -1,14 +1,38 @@
 module HomePage exposing (view)
 
+import Colors
+import Css
 import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes exposing (css)
 import Metadata exposing (Metadata, PageMetadata)
+import ModularScale
 import Pages
 import Pages.PagePath as PagePath exposing (PagePath)
 
 
 view : List ( PagePath Pages.PathKey, Metadata ) -> PageMetadata -> Html msg -> List (Html msg)
 view siteMetadata pageMetadata rendered =
-    [ Html.h1 [] [ Html.text pageMetadata.title ]
+    [ Html.h1
+        [ css
+            [ Css.fontSize (ModularScale.rem 3)
+            , Css.lineHeight (ModularScale.rem 4)
+            , Css.display Css.inlineBlock
+            , Css.paddingRight (ModularScale.rem 0)
+            , Css.paddingLeft (ModularScale.rem 2)
+            , Css.fontFamilies [ "'Exo 2'", "Helvetica", "sans-serif" ]
+            , Css.backgroundImage
+                (Css.linearGradient2
+                    Css.toTop
+                    (Css.stop (Css.rgba 255 255 255 0))
+                    (Css.stop2 (Css.rgba 255 255 255 0) (ModularScale.rem 0))
+                    [ Css.stop2 (Colors.toCss Colors.greenLightest) (ModularScale.rem 0)
+                    , Css.stop2 (Colors.toCss Colors.greenLightest) (Css.rem (ModularScale.scale 0 + ModularScale.scale 1))
+                    , Css.stop2 (Css.rgba 255 255 255 0) (Css.rem (ModularScale.scale 0 + ModularScale.scale 1))
+                    ]
+                )
+            ]
+        ]
+        [ Html.text pageMetadata.title ]
     , rendered
     , Html.text "latest posts"
     ]
