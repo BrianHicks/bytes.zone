f88f7f2b3663a3edff3dbdd0e06f1f1a8c3f27a3
Brian Hicks <brian@brianthicks.com>
2020-02-05 11:13:28 -0600

    make subset.py work with the new format

--- script/subset.py
+++ script/subset.py
@@ -6,50 +6,59 @@ import os.path
 import subprocess
 
 FONTS = {
-    'Exo 2': set(['./dist/fonts/Exo2-Regular.woff2', './dist/fonts/Exo2-Bold.woff2', './dist/fonts/Exo2-BoldItalic.woff2']),
-    'Open Sans': set(['./dist/fonts/OpenSans.woff2', './dist/fonts/OpenSans-Bold.woff2', './dist/fonts/OpenSans-BoldItalic.woff2', './dist/fonts/OpenSans-Italic.woff2']),
-    'Jetbrains Mono': set(['./dist/fonts/Jetbrains-Mono.woff2']),
+    ('Exo 2', '700', 'italic'): './dist/fonts/Exo2-BoldItalic.woff2',
+    ('Exo 2', '700', 'normal'): './dist/fonts/Exo2-Bold.woff2',
+    ('Exo 2', '400', 'normal'): './dist/fonts/Exo2-Regular.woff2',
+    ('Open Sans', '400', 'normal'): './dist/fonts/OpenSans.woff2',
+    ('Open Sans', '400', 'italic'): './dist/fonts/OpenSans-Italic.woff2',
+    ('Open Sans', '700', 'normal'): './dist/fonts/OpenSans-Bold.woff2',
+    ('Open Sans', '700', 'italic'): './dist/fonts/OpenSans-BoldItalic.woff2',
+    ('Jetbrains Mono', '400', 'normal'): './dist/fonts/Jetbrains-Mono.woff2',
 }
 OUTPUT_DIR = './dist'
 
-def glyphhanger(args):
-    args = ['./node_modules/.bin/glyphhanger'] + list(args)
-    return subprocess.check_output(args)
+def codepoint(char):
+    return hex(ord(char))[2:]
 
 def main():
-    files = subprocess.check_output(['find', OUTPUT_DIR, '-name', '*.html']).decode('utf-8').strip().split('\n')
+    files = [os.path.abspath(f) for f in subprocess.check_output(['find', OUTPUT_DIR, '-name', '*.html']).decode('utf-8').strip().split('\n')]
     print('Finding subsets of %d fonts on %d pages' % (len(FONTS), len(files)))
-    glyphhanger_output = glyphhanger(files + ['--onlyVisible', '--json', '--family="%s"' % ','.join(FONTS)]).decode('utf-8')
-    ranges = json.loads(glyphhanger_output)
 
-    for (font, filenames) in FONTS.items():
-        unicodes = ranges.get(font, None)
-        if unicodes is None:
-            print("There weren't any usages of %s according to glyphhanger, skipping." % font)
+    faces_output = subprocess.check_output(['node', './script/faces.js'] + files)
+    faces = json.loads(faces_output)
+
+    for font_and_chars in faces:
+        font = font_and_chars['font']
+        filename = FONTS.get((font['face'], font['weight'], font['style']), None)
+        if filename is None:
+            # not a problem, we just don't care about this face. Stuff like
+            # "sans-serif" and "monospace" shows up here.
             continue
 
-        for filename in filenames:
-            out_filename = filename + '.subset'
-            subprocess.check_call([
-                'pyftsubset',
-                filename,
-                '--unicodes=%s' % unicodes,
-                '--drop-tables=BASE,JSTF,DSIG,EBDT,EBLC,EBSC,SVG,PCLT,LTSH,Feat,Glat,Gloc,Silf,Sill,CBLC,CBDT,sbix,FFTM',
-                '--flavor=woff2',
-                '--output-file=%s' % out_filename,
-            ])
-            
-            before_size = os.path.getsize(filename)
-            after_size = os.path.getsize(out_filename)
+        unicodes = ','.join(codepoint(char) for char in font_and_chars['chars'])
+        
+        out_filename = filename + '.subset'
+        subprocess.check_call([
+            'pyftsubset',
+            filename,
+            '--unicodes=%s' % unicodes,
+            '--drop-tables=BASE,JSTF,DSIG,EBDT,EBLC,EBSC,SVG,PCLT,LTSH,Feat,Glat,Gloc,Silf,Sill,CBLC,CBDT,sbix,FFTM',
+            '--flavor=woff2',
+            '--output-file=%s' % out_filename,
+        ])
+        
+        before_size = os.path.getsize(filename)
+        after_size = os.path.getsize(out_filename)
 
-            print('Subset %s from %d to %d bytes (%.2f%% of original size)' % (
-                filename,
-                before_size,
-                after_size,
-                (float(after_size) / float(before_size)) * 100,
-            ))
+        print('Subset %s from %d to %d bytes (%.2f%% of original size, %d glyphs)' % (
+            filename,
+            before_size,
+            after_size,
+            (float(after_size) / float(before_size)) * 100,
+            len(font_and_chars['chars']),
+        ))
 
-            os.rename(out_filename, filename)
+        os.rename(out_filename, filename)
 
 if __name__ == '__main__':
     main()
